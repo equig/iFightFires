57506212ab1a1c08daf50debbbf9625e61d0c2a5
Ed Quigley <quigleyej1@2ce9225vpc.gcc.edu>
2013-02-05 10:53:00 -0500

    We're gonna have to lose the hobbits later.

---
+++ hobbits.h
@@ -0,0 +1,13 @@
+//
+//  hobbits.h
+//  iFightFires
+//
+//  Created by Ed Quigley on 2/5/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hobbits : NSObject
+
+@end
---
+++ hobbits.m
@@ -0,0 +1,13 @@
+//
+//  hobbits.m
+//  iFightFires
+//
+//  Created by Ed Quigley on 2/5/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import "hobbits.h"
+
+@implementation hobbits
+
+@end
