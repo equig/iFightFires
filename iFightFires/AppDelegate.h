c244472b21bba8fb3750ca161d8973a214611bef
Ed Quigley <quigleyej1@2ce9225vpc.gcc.edu>
2013-02-05 10:47:40 -0500

    Initial Commit

---
+++ iFightFires/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iFightFires
+//
+//  Created by Ed Quigley on 2/5/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
