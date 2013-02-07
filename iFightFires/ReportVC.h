45ff0bf6c536e3c7ec2347bdefba8761aad9226c
Thomas <kanetj1@gcc.edu>
2013-02-07 16:35:55 -0500

    Added report view controller

---
+++ iFightFires/ReportVC.h
@@ -0,0 +1,13 @@
+//
+//  ReportVC.h
+//  iFightFires
+//
+//  Created by Notlob on 2/7/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportVC : UIViewController
+
+@end
---
+++ iFightFires/ReportVC.m
@@ -0,0 +1,38 @@
+//
+//  ReportVC.m
+//  iFightFires
+//
+//  Created by Notlob on 2/7/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import "ReportVC.h"
+
+@interface ReportVC ()
+
+@end
+
+@implementation ReportVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
