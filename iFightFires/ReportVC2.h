7f89ea32539153376283da42a3e08a6ddf8ba8c8
Willard Rockwell <RockwellWF1@gcc.edu>
2013-02-10 16:51:56 -0500

    Adds Report2VC.XIB with split view interface

---
+++ iFightFires/ReportVC2.h
@@ -0,0 +1,13 @@
+//
+//  ReportVC2.h
+//  iFightFires
+//
+//  Created by Willard Rockwell on 2/10/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportVC2 : UIViewController
+
+@end
---
+++ iFightFires/ReportVC2.m
@@ -0,0 +1,38 @@
+//
+//  ReportVC2.m
+//  iFightFires
+//
+//  Created by Willard Rockwell on 2/10/13.
+//  Copyright (c) 2013 Code 4. All rights reserved.
+//
+
+#import "ReportVC2.h"
+
+@interface ReportVC2 ()
+
+@end
+
+@implementation ReportVC2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
